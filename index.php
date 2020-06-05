8a209667187ac2db08b801967006a695f69e63ff
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:16:13 +0200

    exo4

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include'../header.php';
+    $name = "Brian";
+    $age = 22;
+    $question = true;
+    $taile = 165.40;
+?>
+<body>
+    <h1>Exercices 4</h1>
+    <p><?="Bonjour je m'appelle ".$name."j'ai ".$age." ans ".$question." voici ma taille".$taile;?><p>
+</body>
+<?php
+    include'../footer.php';
+?>
+
+
+
+
+
